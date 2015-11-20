59e8c71bde1f40a252f82f83eba67be4ddb8d014
Chris Cuming <ccuming@gmail.com>
2015-11-19 19:02:18 -0500

    #3: Moved Ansible config file to a project subdirectory.

--- env_config.sh
+++ env_config.sh
@@ -3,5 +3,5 @@
 PROJECT_NAME="AnsibleScratch"
 
 source ~/ansible/hacking/env-setup -q
-export ANSIBLE_CONFIG=~/$PROJECT_NAME/ansible.cfg
+export ANSIBLE_CONFIG=~/$PROJECT_NAME/config/ansible.cfg
 export ANSIBLE_INVENTORY=~/$PROJECT_NAME/ansible_hosts
